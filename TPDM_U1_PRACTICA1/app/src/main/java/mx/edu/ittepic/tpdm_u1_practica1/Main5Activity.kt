a029d843db128643fcaa94bf1ab035d13a2c2a44
RRN1211.roriverana@ittepic.edu.mx <47360131+RRN1211@users.noreply.github.com>
2019-09-09 22:59:53 -0600

    PRIMERO

---
+++ TPDM_U1_PRACTICA1/app/src/main/java/mx/edu/ittepic/tpdm_u1_practica1/Main5Activity.kt
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.tpdm_u1_practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Main5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main5)
+
+    }
+}
---
+++ TPDM_U1_PRACTICA1/app/src/main/java/mx/edu/ittepic/tpdm_u1_practica1/Main8Activity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ittepic.tpdm_u1_practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main8Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main8)
+    }
+}
